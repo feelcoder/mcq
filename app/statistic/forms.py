03a4fc4e328feae7b5318394e326d17f806c036f
feelcoder <lea.egoko@yahoo.fr>
2017-08-08 20:19:39 +0100

    statistics updated

---
+++ app/statistic/forms.py
@@ -0,0 +1,11 @@
+# Import Form and RecaptchaField (optional)
+from flask_wtf import Form  # , RecaptchaField
+# Import Form and RecaptchaField (optional)
+from flask_wtf import Form  # , RecaptchaField
+
+# Import Form elements such as TextField and BooleanField (optional)
+from wtforms import TextField, IntegerField, SelectField, DateTimeField, HiddenField, FloatField, SubmitField  # BooleanField
+
+# Import Form validators
+from wtforms.validators import Required, Length
+
